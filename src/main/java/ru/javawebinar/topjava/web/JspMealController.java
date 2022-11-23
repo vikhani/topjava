79a697463eddb3f8f9c6788a1186978f7fc04eb4
vikhani <vikhanieva@gmail.com>
2022-11-23 22:17:57 +0300

    HW6 - Changes according to the review

--- src/main/java/ru/javawebinar/topjava/web/JspMealController.java
+++ src/main/java/ru/javawebinar/topjava/web/JspMealController.java
@@ -4,14 +4,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.service.MealService;
 
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.Objects;
@@ -26,78 +24,72 @@ public class JspMealController {
     @Autowired
     private MealService service;
 
-    private static final String USER_ID = "userId";
     private static final String MEALS = "meals";
 
     @PostMapping("/add")
-    protected void addMeal(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        int userId = getUserId(request);
+    protected String add(HttpServletRequest request) {
+        int userId = SecurityUtil.authUserId();
         log.info("setMeal for user {}", userId);
 
         Meal meal = getMealFromRequest(request);
 
         service.create(meal, userId);
-        response.sendRedirect(MEALS);
+        return MEALS;
     }
 
     @PostMapping("/update")
-    protected void updateMeal(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        int userId = getUserId(request);
+    protected String update(HttpServletRequest request) {
+        int userId = SecurityUtil.authUserId();
         log.info("updateMeal for user {}", userId);
 
         Meal meal = getMealFromRequest(request);
 
         service.update(meal, userId);
-        response.sendRedirect(MEALS);
+        return MEALS;
     }
 
     @GetMapping()
-    protected void getMeal(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
-        int userId = getUserId(request);
+    protected String get(HttpServletRequest request, Model model) {
+        int userId = SecurityUtil.authUserId();
         int id = Integer.parseInt(request.getParameter("id"));
         log.info("getMeal with id={} for user {}", id, userId);
 
-        request.setAttribute(MEALS, service.get(id, userId));
-        request.getRequestDispatcher("/meals").forward(request, response);
+        model.addAttribute("meal", service.get(id, userId));
+        return MEALS;
     }
 
     @GetMapping("/all")
-    protected void getAllMeals(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
-        int userId = getUserId(request);
+    protected String getAll(Model model) {
+        int userId = SecurityUtil.authUserId();
         log.info("getAllMeals for user {}", userId);
 
-        request.setAttribute(MEALS, service.getAll(userId));
-        request.getRequestDispatcher("/meals").forward(request, response);
+        model.addAttribute(MEALS, service.getAll(userId));
+        return MEALS;
     }
 
     @GetMapping("/filtered")
-    protected void getMealsBetweenDates(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
-        int userId = getUserId(request);
+    protected String getBetweenDates(HttpServletRequest request, Model model) {
+        int userId = SecurityUtil.authUserId();
         LocalDate startDate = parseLocalDate(request.getParameter("startDate"));
         LocalDate endDate = parseLocalDate(request.getParameter("endDate"));
         log.info("getMealsBetweenDates for user {}, startDate={}, endDate={}", userId, startDate, endDate);
 
-        request.setAttribute("meal", service.getBetweenInclusive(startDate, endDate, userId));
-        request.getRequestDispatcher("/mealForm").forward(request, response);
+        model.addAttribute(MEALS, service.getBetweenInclusive(startDate, endDate, userId));
+        return "mealForm";
     }
 
     @DeleteMapping()
-    protected void deleteMeal(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        int userId = getUserId(request);
+    protected String delete(HttpServletRequest request) {
+        int userId = SecurityUtil.authUserId();
         int id = getId(request);
         log.info("deleteMeal with id={} for user {}", id, userId);
 
         service.delete(id, userId);
-        response.sendRedirect(MEALS);
+        return MEALS;
     }
 
     private int getId(HttpServletRequest request) {
         String paramId = Objects.requireNonNull(request.getParameter("id"));
-        return Integer.parseInt(paramId);
-    }
-
-    private int getUserId(HttpServletRequest request) {
-        String paramId = Objects.requireNonNull(request.getParameter(USER_ID));
         return Integer.parseInt(paramId);
     }
 
